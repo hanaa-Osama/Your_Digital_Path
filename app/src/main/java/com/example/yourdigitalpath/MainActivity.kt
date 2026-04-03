c496726876a3c6d95e5df77ed43d605d82ba3321
Hanaa Osama <hanaaosama209@gmail.com>
2026-04-03 18:39:38 +0200

    FINISH_CLEAN_ARCHITECTURE_

--- app/src/main/java/com/example/yourdigitalpath/MainActivity.kt
+++ app/src/main/java/com/example/yourdigitalpath/MainActivity.kt
@@ -5,16 +5,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.Text
-import com.example.yourdigitalpath.ui.theme.YourDigitalPathTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            YourDigitalPathTheme {
-                Text("Digital Path")
-            }
+            Text("Digital Path")
         }
     }
 }

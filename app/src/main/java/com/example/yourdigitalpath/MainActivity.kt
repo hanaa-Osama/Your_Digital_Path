202924b45c93f47866ec33195653791ed6fc8d43
AmiraAbdel-fatah <amiraelmezien@gmail.com>
2026-04-10 14:39:04 +0200

    - Resolve Conflicts.
    Signed-off-by: AmiraAbdel-fatah <amiraelmezien@gmail.com>

--- app/src/main/java/com/example/yourdigitalpath/MainActivity.kt
+++ app/src/main/java/com/example/yourdigitalpath/MainActivity.kt
@@ -5,26 +5,21 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-<<<<<<< HEAD
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import com.example.yourdigitalpath.data.dataSource.remote.FirestoreNotificationListener
 import com.example.yourdigitalpath.presentation.FileUploadScreen
 import com.example.yourdigitalpath.presentation.dataEntry.DataScreen
 import com.example.yourdigitalpath.presentation.service_request.ServiceRequestScreen
-=======
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.ui.Modifier
-import com.example.yourdigitalpath.data.dataSource.remote.FirestoreNotificationListener
->>>>>>> development
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
-
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -39,40 +34,36 @@ class MainActivity : ComponentActivity() {
         firestoreNotificationListener.startListening()
 
         setContent {
-<<<<<<< HEAD
-            var currentStep by remember { mutableIntStateOf(1) }
-
-            Scaffold(
-                modifier = Modifier.fillMaxSize(),
-            ) { innerPadding ->
-                when (currentStep) {
-                    1 -> ServiceRequestScreen(
-                        modifier = Modifier.padding(innerPadding),
-                        onNext = { currentStep = 2 },
-                        onBack = { finish() }
-                    )
-
-                    2 -> DataScreen(
-                        modifier = Modifier.padding(innerPadding),
-                        onNext = { currentStep = 3 },
-                        onBack = { currentStep = 1 }
-                    )
-
-                    3 -> FileUploadScreen(
-                        modifier = Modifier.padding(innerPadding),
-                        onNext = { currentStep = 4 },
-                        onBack = { currentStep = 2 }
-                    )
-
-                    else -> {}
-                }
-=======
             MaterialTheme {
                 Surface(modifier = Modifier.fillMaxSize()) {
+                    var currentStep by remember { mutableIntStateOf(1) }
 
+                    Scaffold(
+                        modifier = Modifier.fillMaxSize(),
+                    ) { innerPadding ->
+                        when (currentStep) {
+                            1 -> ServiceRequestScreen(
+                                modifier = Modifier.padding(innerPadding),
+                                onNext = { currentStep = 2 },
+                                onBack = { finish() }
+                            )
+
+                            2 -> DataScreen(
+                                modifier = Modifier.padding(innerPadding),
+                                onNext = { currentStep = 3 },
+                                onBack = { currentStep = 1 }
+                            )
+
+                            3 -> FileUploadScreen(
+                                modifier = Modifier.padding(innerPadding),
+                                onNext = { currentStep = 4 },
+                                onBack = { currentStep = 2 }
+                            )
+
+                            else -> {}
+                        }
+                    }
                 }
-
->>>>>>> development
             }
         }
     }
--- app/src/main/java/com/example/yourdigitalpath/data/dataSource/local/Database.kt
+++ app/src/main/java/com/example/yourdigitalpath/data/dataSource/local/Database.kt
@@ -2,25 +2,26 @@ package com.example.yourdigitalpath.data.dataSource.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.example.yourdigitalpath.data.dataSource.local.Dao.NotificationDao
 import com.example.yourdigitalpath.data.dataSource.local.Dao.ServiceRequestDao
 import com.example.yourdigitalpath.data.dataSource.local.Dao.certificates.BirthCertificateDao
+import com.example.yourdigitalpath.data.dataSource.local.Entity.NotificationEntity
 import com.example.yourdigitalpath.data.dataSource.local.Entity.ServiceRequestEntity
 import com.example.yourdigitalpath.data.dataSource.local.Entity.certificates.BirthCertificateEntity
-import com.example.yourdigitalpath.data.dataSource.local.Dao.NotificationDao
-import com.example.yourdigitalpath.data.dataSource.local.Entity.NotificationEntity
 import com.example.yourdigitalpath.data.local.Dao.OrderDao
 import com.example.yourdigitalpath.data.local.Dao.UserProfileDao
-import com.example.yourdigitalpath.data.local.entity.UserProfileEntity
 import com.example.yourdigitalpath.data.local.entity.OrderEntity
+import com.example.yourdigitalpath.data.local.entity.UserProfileEntity
 
 @Database(entities =
     [
+        NotificationEntity::class,
         UserProfileEntity::class,
         OrderEntity::class,
-        NotificationEntity::class,
-        UserProfileEntity::class, OrderEntity::class, BirthCertificateEntity::class, ServiceRequestEntity::class
+        BirthCertificateEntity::class,
+        ServiceRequestEntity::class
     ],
-    version = 7,
+    version = 5,
     exportSchema = false
 )
 abstract class Database : RoomDatabase() {
